ebabd17f99911f4ec7402e3057b7cfc47235e2d7
WinterThane <demonsaso@gmail.com>
2018-04-08 20:06:12 +0200

    Added input loop

--- Loop/Loop/Program.fs
+++ Loop/Loop/Program.fs
@@ -1,9 +1,19 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 
 [<EntryPoint>]
 let main argv =
+
+    let rec queryAnInt () =
+        printf "Input: " 
+        let input = System.Console.ReadLine()
+        match System.Int32.TryParse input with
+        | (true, number) -> printf "Number: %d was the input\n" number
+        | _              -> match input with
+                            | "a" -> printf "Input was: %s - print all\n" input
+                            | "q" -> printf "Input was: %s - quit\n" input
+                            | _   -> queryAnInt ()
+
+    queryAnInt () |> ignore
 
     printfn "Press any key to exit ..." 
     Console.ReadLine() |> ignore
